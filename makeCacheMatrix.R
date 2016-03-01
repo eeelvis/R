f37482d5ff2e2adcbb8ef6c0d1a874c6c84e86e6
eeelvis <elvis.mcp@gmail.com>
2016-03-01 00:35:05 -0300

    Update makeCacheMatrix.R
    Updates

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,19 +1,21 @@
+# I am use the structure as makeVector() and cachemean()
+# Build 2 functions makeCacheMatrix() and cacheSolve()
+# Example:
+# > source("cachematrix.R")
+# > matrixA<-makeCacheMatrix(matrix(c(1:8,10),3,3))
+# > cacheSolve(matrixA)
+# > cacheSolve(matrixA)
+# Cache when calling cacheSolve() the second time will be used
 
-makeCacheMatrix <- function(m = matrix()) {
-
+# makeCacheMatrix() build a list that stores a matrix and its inverse  
+makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
-        
-        set <- function(y){
-                m <<- y
+        set <- function(y) {
+                x <<- y
                 inv <<- NULL
         }
-        
-        get <- function() m
-        
-        setInv = function(solve) inv <<- solve
-        
-        getInv = function() inv
-        
-        list(set = set, get = get, setInv = setInv, getInv = getInv)
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
-
